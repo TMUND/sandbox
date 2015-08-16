ea39a5c7ecdaa6062d236ac8c6f2875d839716cb
Toni Munder <tonimunder@hotmail.com>
2015-08-16 13:27:25 -0700

    Setup

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['src/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    jshint: {
+      options: {
+          jshintrc: '.jshintrc'
+      }
+    }
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Task list
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+
+};
