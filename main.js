9bbc50c3dc3cc17f6ba983ed6a2da38d2fedad1b
Toni Munder <tonimunder@hotmail.com>
2015-08-16 15:47:59 -0700

    Messing around with buttons and panels using bootstrap

--- main.js
+++ main.js
@@ -3,11 +3,34 @@
   window.App = {
     // ----------
     init: function() {
+      $('.btn1').on('mouseover', function() {
+        $('#panel1').fadeToggle(500);
+      });
+
+      $('.btn2').on('mouseover', function() {
+        $('#panel2').fadeToggle(500);
+      });
+
+      $('.btn3').on('mouseover', function() {
+        $('#panel3').fadeToggle(500);
+      });
+
+      $('.btn4').on('click', function() {
+        $('#panel4').fadeToggle(500);
+      });
+
+      $('.btn5').on('click', function() {
+        $('#panel5').fadeToggle(500);
+      });
+
+      $('.btn6').on('click', function() {
+        $('#panel6').fadeToggle(500);
+      });
     }
   };
 
+  // ----------
   $(document).ready(function() {
     App.init();
   });
-
 })();
