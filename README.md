d226a2093841425bd0e4bbb7582cc933cd1f5d57
Toni Munder <tonimunder@hotmail.com>
2015-08-22 10:24:37 -0700

    Created a working tab panel

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 TODO:
 
 * Some stuff
-* Create a gh-pages page
-* Create a working tab bar
 * Create an image slider
--- main.js
+++ main.js
@@ -3,6 +3,9 @@
   window.App = {
     // ----------
     init: function() {
+
+      // Hovers and Clicks Practice
+      // ----------
       $('.hoverbutton').on('mouseover', function() {
         var panelId = $(this).attr('data-panelid');
         $('#' + panelId).fadeToggle(500);
@@ -14,6 +17,27 @@
         var panelId = $(this).attr('data-panelid');
         $('#' + panelId).toggle();
         $('#' + panelId + ' .panel-body').html(content);
+      });
+
+      // Panel Sliding Up and Down Practice
+      // ----------
+        $('.tab-panels .tabs li').on('click', function() {
+        var $panel = $(this).closest('.tab-panels');
+
+        $panel.find('.tabs li.active').removeClass('active');
+        $(this).addClass('active');
+
+        $panel.find('.tab-panel.active').slideUp(500, showNextPanel);
+
+        var panelToShow = $(this).attr('rel');
+
+        function showNextPanel() {
+          $(this).removeClass('active');
+
+          $('#' + panelToShow).slideDown(500, function() {
+            $(this).addClass('active');
+          });
+        }
       });
     }
   };
